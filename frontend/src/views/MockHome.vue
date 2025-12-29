c80e3b57bc7008b41af017f79a557ef11fdf5f47
Marco Galeri <marco.galeri@studio.unibo.it>
2025-12-29 19:31:38 +0100

    feat: Add a Nav bar and a theme switcher

---
+++ frontend/src/views/MockHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <base-layout>
+      <div style="padding: 20px; text-align: center;">
+        <h1>Test Navbar Mode</h1>
+        <p>Scorri per testare l'effetto blur!</p>
+        <p v-for="i in 20" :key="i">Riga di contenuto {{ i }}</p>
+        <p v-for="i in 50" :key="i">
+          <ion-button>
+            Riga di contenuto {{ i }}
+          </ion-button>
+        </p>
+      </div>
+  </base-layout>
+
+</template>
+
+<script setup lang="ts">
+import BaseLayout from "@/layout/BaseLayout.vue";
+</script>

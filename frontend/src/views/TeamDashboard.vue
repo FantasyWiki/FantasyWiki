da93099cd68b9e02f0273b40239bece62f22a970
Marco Galeri <marco.galeri@studio.unibo.it>
2025-12-29 19:32:46 +0100

    feat(dashboard): WIP title and cards summary

---
+++ frontend/src/views/TeamDashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <BaseLayout>
+    <ion-text color="secondary">
+      <h1>{{ $route.params.teamId }} Dashboard</h1>
+    </ion-text>
+    <ion-text color="tertiary">
+    <p>Manage your team, track performance, and climb the leaderboard</p>
+    </ion-text>
+    <TeamDashboardCards></TeamDashboardCards>
+  </BaseLayout>
+</template>
+
+<script setup lang="ts">
+import { IonText, } from '@ionic/vue';
+import BaseLayout from "@/layout/BaseLayout.vue";
+import TeamDashboardCards from "@/modules/TeamDashboard/TeamDashboardCards.vue";
+</script>
+
+<style scoped>
+
+</style>

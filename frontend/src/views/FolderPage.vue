9cad90b178554266316feb20f9819b72e3f00c27
Luca Patrignani <luca0patrignani@gmail.com>
2026-01-05 15:44:39 +0100

    feat: create searchbar

--- frontend/src/views/FolderPage.vue
+++ frontend/src/views/FolderPage.vue
@@ -1,31 +1,23 @@
 <template>
-  <ion-page>
-    <ion-section>
-      <h1>Build Your <strong>Knowledge Empire</strong></h1>
-      <ion-content>
-        Draft Wikipedia articles, earn points from real search trends, and compete in weekly tournaments. The ultimate fantasy game for the curious mind.
-      </ion-content>
-    </ion-section>
-  </ion-page>
+  <ion-content class="ion-padding-horizontal ion-text-center">
+    <h1 class="ion-margin-horizontal">Build Your <strong>Knowledge Empire</strong></h1>
+    <p class="ion-margin-horizontal ion-padding-horizontal">
+      Draft Wikipedia articles, earn points from real search trends, and compete in weekly tournaments. The ultimate
+      fantasy game for the curious mind.
+    </p>
+    <ion-item-group class="ion-margin-horizontal ion-padding-horizontal ion-display-flex ion-justify-content-center">
+      <ion-searchbar :animated=true placeholder="Search articles... (e.g., Bitcoin, AI)"
+        color="light ion-text-start"></ion-searchbar>
+      <ion-button class="ion-text-nowrap ion-text-capitalize">
+        Get Started
+        <ion-icon slot="end" :icon="arrowForwardOutline"></ion-icon>
+      </ion-button>
+    </ion-item-group>
+  </ion-content>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonPage } from '@ionic/vue';
+import { arrowForwardOutline } from 'ionicons/icons';
 </script>
 
-<style scoped>
-ion-section {
-  text-align: center;
-}
-
-h1 {
-  font-size: xx-large;
-  font-family: 'Libre Baskerville', serif;
-  font-weight: bold;
-  color: black;
-}
-
-strong {
-  color: var(--ion-color-primary);
-}
-</style>
+<style scoped></style>

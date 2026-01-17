794d304a349802af721cb3e51a93781538bea688
Luca Patrignani <luca0patrignani@gmail.com>
2026-01-17 17:29:21 +0100

    refactor: move article leaderboard in a new component

---
+++ frontend/src/views/home-page/ArticleLeaderboard.vue
@@ -0,0 +1,77 @@
+<template>
+  <ion-card class="ion-padding">
+    <ion-card-header class="ion-no-padding ion-margin-bottom">
+      <div class="ion-display-flex ion-justify-content-between">
+        <ion-card-title class="ion-text-start">
+          Top articles this week
+        </ion-card-title>
+        <ion-text color="secondary">
+          +12.5% today
+        </ion-text>
+      </div>
+    </ion-card-header>
+    <ion-card-content class="ion-text-center ion-no-padding">
+      <ion-list>
+        <ion-item v-for="n in 5" :key="n" class="ion-margin-bottom ion-no-padding">
+          <ion-badge class="ion-margin ion-padding">
+            <ion-icon :icon="trendingUpOutline"></ion-icon>
+          </ion-badge>
+          <ion-label class="ion-text-start">
+            <h3 class="ion-no-margin">Article Title {{ n }}</h3>
+            <p class="ion-no-margin">Brief description of the article...</p>
+          </ion-label>
+          <ion-badge slot="end" color="primary">+{{ n * 100 }} pts</ion-badge>
+        </ion-item>
+      </ion-list>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script setup lang="ts">
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonText,
+  IonBadge,
+  IonIcon
+} from '@ionic/vue';
+import { trendingUpOutline } from 'ionicons/icons';
+</script>
+
+<style scoped>
+ion-card {
+  border-radius: 1rem;
+  /* There is no way to do this in Ionic */
+  --ion-padding: 1.5rem;
+}
+
+ion-card-title {
+  font-weight: bold;
+  --ion-margin: 1rem;
+}
+
+ion-card-header ion-text {
+  font-weight: 500;
+}
+
+ion-item {
+  --border-style: none;
+  --background: var(--ion-background-color);
+  --border-radius: 0.5rem;
+}
+
+ion-item ion-badge {
+  --ion-margin: 0.5rem;
+  --ion-padding: 0.5rem;
+  --background: var(--ion-background-color-step-450);
+}
+
+ion-badge ion-icon {
+  font-size: 1.3rem;
+}
+</style>

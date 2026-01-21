17c0a623de0b9e35aa97623d607d6c7f40997fe4
Marco Galeri <marco.galeri@studio.unibo.it>
2026-01-21 18:03:03 +0100

    ci: fix linting error

--- frontend/src/tests/HomePage.spec.ts
+++ frontend/src/tests/HomePage.spec.ts
@@ -1,15 +1,22 @@
-import { describe, it, expect } from "vitest";
+import {describe, it, expect, beforeEach} from "vitest";
 import { mount } from "@vue/test-utils";
 import router from "@/router/index";
 import HomePage from "@/views/HomePage.vue";
+import {createPinia} from "pinia";
 
 describe("HomePage.vue", () => {
+  let pinia;
+
+  beforeEach(() => {
+    // Create a fresh Pinia instance for each test
+    pinia = createPinia();
+  });
   it("should mount without any console errors or warnings", () => {
     router.push("/");
     router.isReady().then(() => {
       const wrapper = mount(HomePage, {
         global: {
-          plugins: [router],
+          plugins: [router, pinia],
         },
       });
 

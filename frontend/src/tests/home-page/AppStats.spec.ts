808294da9d3cde6b68b3fdaeb6cab75b4ea0b328
Luca Patrignani <luca0patrignani@gmail.com>
2026-01-22 09:24:51 +0100

    chore: move HomePage tests to its directory and rename tests for stats

--- frontend/src/tests/home-page/AppStats.spec.ts
+++ frontend/src/tests/home-page/AppStats.spec.ts
@@ -3,7 +3,7 @@ import { mount } from '@vue/test-utils';
 import router from "@/router/index";
 import AppStats from '@/views/home-page/AppStats.vue';
 
-describe('home-page/stats.vue', () => {
+describe('AppStats.vue', () => {
   it('should mount without any console errors or warnings', async () => {
     router.push("/");
     await router.isReady();

8d8c54d33d1db8ddf491c668b8e7ed320f9c3e2c
luca-patrignani <92518571+luca-patrignani@users.noreply.github.com>
2026-02-13 17:17:59 +0100

    ci: move node and npm versions from libs.version.toml to package.json (#91)

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,11 +1,16 @@
+import groovy.json.JsonSlurper
+
 plugins {
     alias(libs.plugins.node)
 }
 
+val packageJson = JsonSlurper().parse(file("package.json")) as Map<*, *>
+val engines = packageJson["engines"] as Map<*, *>
+
 node {
     download = true
-    version = libs.versions.node
-    npmVersion = libs.versions.npm
+    version = engines["node"] as String
+    npmVersion = engines["npm"] as String
     npmInstallCommand = "ci"
 }
 
@@ -14,6 +19,6 @@ tasks.register("check") {
         "npm_audit",
         "npm_run_format",
         "npm_run_lint",
-        "npm_run_test-unit"
+        "npm_run_test-unit",
     )
 }

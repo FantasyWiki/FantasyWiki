140cfc140fbb6d0ae4abd164c92eae9a270d8312
Luca Patrignani <luca0patrignani@gmail.com>
2026-03-02 16:42:29 +0100

    ci: create new task dev for starting both frontend and backend

---
+++ backend/build.gradle.kts
@@ -0,0 +1,29 @@
+import com.github.gradle.node.npm.task.NpmTask
+import groovy.json.JsonSlurper
+
+plugins {
+    alias(libs.plugins.node)
+}
+
+val packageJson = JsonSlurper().parse(file("package.json")) as Map<*, *>
+val engines = packageJson["engines"] as Map<*, *>
+
+node {
+    download = true
+    version = engines["node"] as String
+    npmVersion = engines["npm"] as String
+    npmInstallCommand = "ci"
+}
+
+tasks.named<NpmTask>("npm_audit") {
+    args.set(listOf("--omit=dev"))
+}
+
+tasks.register("check") {
+    dependsOn(
+        "npm_audit",
+        "npm_run_format",
+        "npm_run_lint",
+        "npm_run_test-unit",
+    )
+}

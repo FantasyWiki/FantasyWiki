0124e86d6fe31e09b3de210107c39944b6a11ae7
Luca Patrignani <luca0patrignani@gmail.com>
2026-03-07 12:25:45 +0100

    ci: add eslint to backend

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -3,6 +3,7 @@ import { GoogleProfile } from "../types";
 // Extend Express Request to carry the decoded JWT user
 declare global {
   namespace Express {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface User extends GoogleProfile {}
   }
 }

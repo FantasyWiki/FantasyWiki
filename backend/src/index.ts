1a2f90892bc712e3f406b782b4119b027d384e29
Luca Patrignani <luca0patrignani@gmail.com>
2026-03-21 10:35:28 +0100

    refactor: recreate authentication via google oauth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,36 @@
 import { Hono } from "hono";
+import { googleAuth } from "@hono/oauth-providers/google";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Bindings }>();
+
+type Bindings = {
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+};
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.use("/auth/google", async (c, next) => {
+  const handler = googleAuth({
+    client_id: c.env.GOOGLE_CLIENT_ID,
+    client_secret: c.env.GOOGLE_CLIENT_SECRET,
+    scope: ["openid", "email", "profile"],
+  });
+  return handler(c, next);
+});
+
+app.get("/auth/google", (c) => {
+  const token = c.get("token");
+  const grantedScopes = c.get("granted-scopes");
+  const user = c.get("user-google");
+
+  return c.json({
+    token,
+    grantedScopes,
+    user,
+  });
 });
 
 export default app;

7ed196ba506430c9a49aa371525a67e5a0c2c408
Luca Patrignani <luca0patrignani@gmail.com>
2026-03-21 10:35:28 +0100

    refactor: recreate authentication via google oauth

--- frontend/src/views/auth/LoginPage.vue
+++ frontend/src/views/auth/LoginPage.vue
@@ -38,7 +38,7 @@ import AppLogo from "@/views/AppLogo.vue";
 
 const route = useRoute();
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:3000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const errorMessage = computed(() =>
   route.query.error === "auth_failed"

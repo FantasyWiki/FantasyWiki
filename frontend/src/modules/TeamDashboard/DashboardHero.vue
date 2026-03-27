efef8a6cdf60bc1401062017db7e328b3eac804e
Marco Galeri <marco.galeri@studio.unibo.it>
2026-03-27 15:31:39 +0100

    test: moved and splitted test

--- frontend/src/modules/TeamDashboard/DashboardHero.vue
+++ frontend/src/modules/TeamDashboard/DashboardHero.vue
@@ -129,7 +129,7 @@
                   <!-- Dot indicators -->
                   <div class="carousel-dots">
                     <button
-                      v-for="i in allStats.length"
+                      v-for="(_,i) in allStats"
                       :key="i"
                       class="dot"
                       :class="{ 'dot--active': activeIndex === i }"

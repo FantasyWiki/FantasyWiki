25dfcc428a90fc629a62cdf098f4277683ce4bec
Luca Patrignani <luca0patrignani@gmail.com>
2026-03-27 17:34:35 +0100

    feat: expose interfaces defined by model

---
+++ model/index.ts
@@ -0,0 +1,5 @@
+export { Contract } from "./contract";
+export { League } from "./league";
+export { Notification } from "./notification";
+export { Player } from "./player";
+export { Team } from "./team";

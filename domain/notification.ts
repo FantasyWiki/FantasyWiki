5284907a23c5f3c381446e90a6cacea9146f838e
Luca Patrignani <luca0patrignani@gmail.com>
2026-03-27 17:34:35 +0100

    feat: expose interfaces defined by model

--- domain/notification.ts
+++
@@ -1,9 +0,0 @@
-import { Temporal } from "@js-temporal/polyfill";
-
-export interface Notification {
-  id: string;
-  contractId: string;
-  message: string;
-  date: Temporal.PlainDate;
-  isRead: boolean;
-}
--- domain/team.ts
+++
@@ -1,7 +0,0 @@
-export interface Team {
-  id: string;
-  name: string;
-  playerId: string;
-  leagueId: string;
-  credits: number;
-}

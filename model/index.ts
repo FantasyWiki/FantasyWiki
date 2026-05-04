88fe33726654dcd7701db7680fe814b624bdc475
Marco Galeri <marco.galeri@studio.unibo.it>
2026-05-04 18:49:02 +0200

    fix: eslint in model

--- model/index.ts
+++
@@ -1,6 +0,0 @@
-export type { Contract } from "./contract";
-export type { League } from "./league";
-export type { Notification } from "./notification";
-export type { Player } from "./player";
-export type { Team } from "./team";
-export * from "./wikimedia";
